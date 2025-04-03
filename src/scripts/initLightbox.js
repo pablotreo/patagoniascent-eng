ee7880a803a41b5fd434dbf05670604afd16d672
Pablo Treo <pablotreo@gmail.com>
2025-04-03 10:37:33 -0300

    algo mas ligthbox

---
+++ src/scripts/initLightbox.js
@@ -0,0 +1,14 @@
+// src/scripts/initLightbox.js
+export async function initLightbox() {
+  if (typeof window === 'undefined') return;
+
+  const PhotoSwipeLightbox = (await import('photoswipe/lightbox')).default;
+
+  const lightbox = new PhotoSwipeLightbox({
+    gallery: '#lightbox-gallery',
+    children: 'a',
+    pswpModule: () => import('photoswipe'),
+  });
+
+  lightbox.init();
+}
